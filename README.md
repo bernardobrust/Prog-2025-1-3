b9f0f790e205bafea9ed9f5a4caec254e1ef7688
Bernardo Brust <bernardo.toledo.pereira@gmail.com>
2025-06-02 18:10:55 -0300

    Pequenas correções

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sla, algm muda isso depois
 
 > Tamo dependendo do fernando pra essa
+
+Abram PR ou issue qualquer coisa
--- src/main.c
+++ src/main.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 
 #include <astf.h>
 
-int test_fn(int x) {
-    return x * x;
-}
+int test_fn(int x) { return x * x; }
 
-void run() {
-    printf("Running!\n");
-}
+void run() { printf("Running!\n"); }
 
 void test() {
-    astf_start_test_suite("Some test");
+  astf_start_test_suite("Some test");
 
-    astf_AE_int(test_fn(3), 9);
-    astf_AE_int(test_fn(4), 16);
-    astf_AE_int(test_fn(5), 25);
+  astf_AE_int(test_fn(3), 9);
+  astf_AE_int(test_fn(4), 16);
+  astf_AE_int(test_fn(5), 25);
 
-    astf_retrieve_results();
+  astf_retrieve_results();
 }
 
 int main(int argc, char *argv[]) {
-    if (argc >= 2) test();
-    else run();
+  if (argc >= 2) {
+    test();
+  } else {
+    run();
+  }
 
-    return 0;
-} 
+  return 0;
+}
