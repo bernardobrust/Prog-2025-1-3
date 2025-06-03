c75ebe3f6f95698d44292fbeeb1f5bdaca561b96
Bernardo Brust <bernardo.toledo.pereira@gmail.com>
2025-06-03 07:44:27 -0300

    Pequenas melhorias

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <astf.h>
 
@@ -17,7 +18,7 @@ void test() {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc >= 2) {
+  if (argc > 1 && !strcmp(argv[1], "test")) {
     test();
   } else {
     run();
