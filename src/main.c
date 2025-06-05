20a2b76364251121eca3c5d12fb70dae07e1fab6
Bernardo Brust <bernardo.toledo.pereira@gmail.com>
2025-06-05 07:46:46 -0300

    Manutenção e comentários

--- src/main.c
+++ src/main.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <astf.h>
+#include "astf.h"
 
 #define BUFFER_SIZE 100 // tamanho máximo de uma palavra
 
-int test_fn(int x) { return x * x; }
+#define FILE1 FILE *test1 = fopen("../test-files/test1.txt", "r")
+#define FILE2 FILE *test2 = fopen("../test-files/test2.txt", "r")
+#define FILE3 FILE *test3 = fopen("../test-files/test3.txt", "r")
+#define FILE4 FILE *test4 = fopen("../test-files/test4.txt", "r")
+#define FILE5 FILE *test5 = fopen("../test-files/test5.txt", "r")
 
+/*
+  * Função responsável pela execução do programa
+  * Fazer leitura, processamento e input aqui
+*/
 void run() { printf("Running!\n"); }
 
-void test() {
+/*
+  * Função responssável por testes
+  * Testar o código aqui
+*/
+int test() {
   astf_start_test_suite("Read tests");
 
-  FILE *test1 = fopen("../test-files/test1.txt", "r");
+  // Para abrir cada arquivo, basta digitar essa linha com o devido número
+  FILE1;
+
+  if (test1 == NULL) {
+    printf("Erro na leitura do arquivo!\n");
+    return 1;
+  }
 
   char word[BUFFER_SIZE];
+  // Loop que lê o arquivo palavra por palavra
   while (fscanf(test1, "%s", word) == 1) {
     printf("%s\n", word);
   }
@@ -23,6 +42,10 @@ void test() {
   astf_AE_string(word, "testes.");
 
   astf_retrieve_results();
+
+  fclose(test1);
+
+  return 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -31,6 +54,4 @@ int main(int argc, char *argv[]) {
   } else {
     run();
   }
-
-  return 0;
 }
