7657595ee3ee627e24991d25e42a98bd054ab3a6
Bernardo Brust <bernardo.toledo.pereira@gmail.com>
2025-06-09 17:09:12 -0300

    Aprimoramento do código e upddate do readme

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 > Tamo dependendo do fernando pra essa
 
 Abram PR ou issue qualquer coisa
+
+Funções:
+- João cuida da contagem de palavras (função count_ocurences)
+- Jonathan cuida da saída de dados (mostra a contagem, apresenta count e os arquivos)
+- Cauã cuida da tolerância (função q vc já fez)
+- Miguel cuida da entrada (recebe o nome do arquivo, palavra de busca)
+- Matheus cuida dos testes (escreve os testes e faz o slide)
--- src/main.c
+++ src/main.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
+// Micro framework de teste
 #include "astf.h"
 
-#define BUFFER_SIZE 100 // tamanho máximo de uma palavra
+#define BUFFER_SIZE 100 // Tamanho máximo de uma palavra
 
 #define FILE1 FILE *test1 = fopen("../test-files/test1.txt", "r")
 #define FILE2 FILE *test2 = fopen("../test-files/test2.txt", "r")
 #define FILE3 FILE *test3 = fopen("../test-files/test3.txt", "r")
 #define FILE4 FILE *test4 = fopen("../test-files/test4.txt", "r")
 #define FILE5 FILE *test5 = fopen("../test-files/test5.txt", "r")
+
+void run();
+int test();
+int count_ocurences(char target[], FILE *file);
+
+int main(int argc, char *argv[]) {
+  if (argc > 1 && !strcmp(argv[1], "test")) {
+    test();
+  } else {
+    run();
+  }
+}
+
+/*
+ * Função responsável por contar quantas ocorrências da
+ * palavra tem num arquivo
+*/
+int count_ocurences(char target[], FILE *file) {
+  int count = 0;
+
+  char word[BUFFER_SIZE];
+  // Loop que lê o arquivo palavra por palavra
+  while (fscanf(file, "%s", word) == 1) {
+    printf("%s\n", word);
+
+    // Verificando se target e word batem
+    if (strcmp(target, word) == 0) {
+      printf("Bateu!");
+    }
+  }
+
+  return count;
+}
 
 /*
   * Função responsável pela execução do programa
@@ -22,36 +56,23 @@ void run() { printf("Running!\n"); }
   * Testar o código aqui
 */
 int test() {
+  // Inicializanddo os teste
   astf_start_test_suite("Read tests");
 
   // Para abrir cada arquivo, basta digitar essa linha com o devido número
   FILE1;
 
-  if (test1 == NULL) {
-    printf("Erro na leitura do arquivo!\n");
-    return 1;
-  }
+  // Conferindo que o arquivo existe
+  astf_AN_null(test1);
 
-  char word[BUFFER_SIZE];
-  // Loop que lê o arquivo palavra por palavra
-  while (fscanf(test1, "%s", word) == 1) {
-    printf("%s\n", word);
-  }
+  // Verificando se "qualquer coisa que não tem num arquivo" não ocorre em "test1.txt"
+  astf_AE_int(0, count_ocurences("qualquer coisa que não tem num arquivo", test1));
 
-  // verifica se a ultima palavra bate
-  astf_AE_string(word, "testes.");
-
+  // Verificando o resultado dos testes
   astf_retrieve_results();
 
+  // Fechando o arquivo
   fclose(test1);
 
   return 1;
-}
-
-int main(int argc, char *argv[]) {
-  if (argc > 1 && !strcmp(argv[1], "test")) {
-    test();
-  } else {
-    run();
-  }
 }
